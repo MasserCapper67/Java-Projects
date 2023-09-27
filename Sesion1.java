f2863879800955b2131fd8f2915cba389d9a736a
Masser <48176717+MasserCapper67@users.noreply.github.com>
2023-09-27 09:58:10 +0200

    Add files via upload
    School trash

---
+++ Sesion1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Sesion1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice; // Primera elección
+        int choice2; // Segunda elección
+        int compare1; // Tercera elección
+        int compare2; // Cuarta elección
+
+        System.out.println("Seleccione un número del 1 al 5 para comenzar el programa: ");
+
+        System.out.println("1. Dibujar un triángulo rectángulo ");
+        System.out.println("2. Dibujar un cuadrado: ");
+        System.out.println("3. Multiplicar dos números: ");
+        System.out.println("4. Escribir una tabla de multiplicar: ");
+        System.out.println("5. Finalizar la ejecución");
+
+
+        choice = sc.nextInt();
+
+        while (!(choice >0 && choice <=5)){
+            System.out.println("Por favor, seleccione un número correcto");
+            choice = sc.nextInt();
+        }
+        System.out.println("Ha seleccionado la opción " + choice);
+
+        System.out.println("Ahora escoja un número entre 0 y 100: ");
+        choice2 = sc.nextInt();
+
+        while (!(choice2>=0 && choice2 <101)){
+            System.out.println("Por favor, seleccione un número correcto");
+            choice2 = sc.nextInt();
+        }
+        System.out.println("Ha seleccionado el número " + choice2);
+
+        System.out.println("Elija dos números enteros. Estos se compararán y se mostrará el mayor de ellos: ");
+        System.out.println("Además, se comprobará si el segundo número es múltiplo del primero: ");
+
+        compare1 = sc.nextInt();
+        compare2 = sc.nextInt();
+
+        if ((compare1 <= compare2) && (compare2 % compare1 == 0)){
+            System.out.println("El número 1 es menor o igual que el número 2 y este es múltiplo del primero");
+            }
+        else System.out.println("El número 1 no es menor o igual que el número 2 o este no es múltiplo del primero");
+    }
+}
---
+++ Sesion2.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Sesion2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+
+        // Triángulo
+
+        System.out.println("Introduzca un número del 5 al 30");
+        System.out.println("Este representará la altura de su triángulo");
+
+        numero = sc.nextInt();
+
+        while (!(numero>5 && numero<30)){
+            System.out.println("Introduzca un número correcto: ");
+            numero = sc.nextInt();
+        }
+        final char asterisco = '*';
+
+        for (int k = 0; k < numero; k++){
+
+            for (int i = 0; i < k; i++){
+                System.out.print(asterisco + " ");
+            };
+            System.out.println(asterisco);
+        }
+
+        // Cuadrado
+
+        System.out.println("Introduzca un número entre 10 y 35");
+        System.out.println("Este corresponderá al lado del cuadrado que se va a dibujar");
+
+        numero = sc.nextInt();
+
+        while (numero < 10 || numero > 35){
+            System.out.println("Introduzca un número correcto: ");
+            numero = sc.nextInt();
+        }
+        for (int k = 0; k < numero; k++){
+            for (int i = 0; i < numero; i++){
+                System.out.print(asterisco + "  ");
+            }
+            System.out.println();
+        }
+
+        // Multiplicación con bucle
+
+        int num1, num2;
+        System.out.println("Introduzca dos números entre 0 y 20:");
+
+        num1 = sc.nextInt();
+
+        while (num1 < 0 || num1 > 20){
+            System.out.println("Por favor, introduzca un número entre 0 y 20:");
+            num1 = sc.nextInt();
+        }
+
+        System.out.println("Introduzca otro número entre 0 y 20:");
+        num2 = sc.nextInt();
+
+        while (num2 < 0 || num2 > 20){
+            System.out.println("Por favor, introduzca un número entre 0 y 20:");
+            num2 = sc.nextInt();
+        }
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+
+
+        // Tabla de multiplicar
+
+        System.out.println("Introduzca un número del 1 al 10/nSe mostrará la tabla de multiplicar de dicho número.");
+        numero = sc.nextInt();
+
+        while(numero < 1 || numero > 10){
+            System.out.println("Introduzca un número del 1 al 10");
+            numero = sc.nextInt();
+            }
+        for (int k = 1; k <= 10; k++){
+            System.out.println(numero + " X " + k + " = " + (numero * k));
+        }
+    }
+}

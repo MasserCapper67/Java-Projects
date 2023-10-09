5ecfe0ea761cb99c5cd16a2bbd0dd7b079b3861c
Masser <48176717+MasserCapper67@users.noreply.github.com>
2023-10-09 08:41:41 +0200

    Sesion4.java

--- src/Sesion4.java
+++ src/Sesion4.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Sesion4 {
     public static Scanner sc = new Scanner(System.in);
+    private static String green = "\u001B[32m"; // Uso el color verde para que el resultado sea más vistoso
+    private static String normal = "\u001B[0m";
 
     public static char leerCharacter(char c){
         while (Character.isLetterOrDigit(c)){
@@ -27,18 +29,24 @@ public class Sesion4 {
         else{
             trianguloRecursivo(base - 1, caracter);
             lineaRecursivo(base, caracter);
-            }
         }
+    }
+    
+    public static int multiplicarRecursivo(int x, int y){
+        if (x == 0 || y == 0){
+            return 0;
+        } else if (y == 1) {
+            return x;
+        }
+        else {
+            return x + multiplicarRecursivo(x, y - 1);
+        }
+    }
 
-
-    public static void main(String[] args) {
-        System.out.println("Introduzca un número: ");
-        int chNum = sc.nextInt();
-        System.out.println("Introduzca un carácter: ");
-        char chChar = sc.next().charAt(0);
-        chChar = leerCharacter(chChar);
-        lineaRecursivo(chNum, chChar);
-
-        trianguloRecursivo(5, '*');
+    public static void tablaMultiplicar(int x){
+        for (int k = 1; k <= 10; k++){
+            System.out.print(x + " X " + k + " = ");
+            System.out.println(green + multiplicarRecursivo(x, k) + normal);
+        }
     }
 }

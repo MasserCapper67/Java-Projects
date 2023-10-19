6f6ff5ddf0bcac19283147fcd38263a327da9dc2
Masser <48176717+MasserCapper67@users.noreply.github.com>
2023-10-19 12:28:07 +0200

    Update BuscarElementoArray.java

--- Element Search Array/BuscarElementoArray.java
+++ Element Search Array/BuscarElementoArray.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
 
-public class BuscarElementoArray{
+public class BuscarElementoArray {
     private static Scanner sc;
-    public static void buscarElemento(int[] array, int elemento, int indice, int contador){
-        if (indice < array.length){
-            if (array[indice] == elemento){
+
+    public static void buscarElemento(int[] array, int elemento, int indice, int contador) {
+        if (indice < array.length) {
+            if (array[indice] == elemento) {
                 System.out.println("Se ha encontrado un elemento " + "'" + elemento + "'" + " en la posición " + "[" + indice + "]");
                 contador++;
             }
             buscarElemento(array, elemento, indice + 1, contador);
-        }else{
-            if (contador == 0){
+        } else {
+            if (contador == 0) {
                 System.out.println("No se ha encontrado ningún elemento coincidente en el array.");
-            }else System.out.println("El total de elementos "  + "(" + elemento + ")" + " coincidentes es: " + contador);
+            } else
+                System.out.println("El total de elementos " + "(" + elemento + ")" + " coincidentes es: " + contador);
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] array = {1, 1, 2, 3, 3, 5, 6, 3, 8, 1, 1, 2};
+        System.out.println("Introduzca el número que desees encontrar. \nEl programa se seguirá ejecutando hasta que introduzcas '-1'");
         int choice = sc.nextInt();
-        buscarElemento(array, choice, 0, 0);
+        while (choice != -1) {
+            buscarElemento(array, choice, 0, 0);
+            choice = sc.nextInt();
+        }
     }
 }
